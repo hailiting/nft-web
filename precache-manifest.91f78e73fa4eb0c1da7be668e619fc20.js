d40ff7718e4efe98ce432672a04d9112b91b0ba7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-09-09 07:13:41 +0000

    Deploy hailiting/nft-web to github.com/hailiting/nft-web.git:gh-pages

---
+++ precache-manifest.91f78e73fa4eb0c1da7be668e619fc20.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d52995fcbbde9679fc39cf2b4a0562",
+    "url": "/nft-web/index.html"
+  },
+  {
+    "revision": "c84982c06fcb1777dbef",
+    "url": "/nft-web/static/css/1.91f2ea11.chunk.css"
+  },
+  {
+    "revision": "250cc7633b254c7d1bff",
+    "url": "/nft-web/static/css/5.06d12aee.chunk.css"
+  },
+  {
+    "revision": "5f65b350d2a7600641da",
+    "url": "/nft-web/static/css/6.e4570bac.chunk.css"
+  },
+  {
+    "revision": "74175e83c49031d4b5b4",
+    "url": "/nft-web/static/css/7.def32f94.chunk.css"
+  },
+  {
+    "revision": "de8477a275d1bc719f2e",
+    "url": "/nft-web/static/css/main.7c0360c2.chunk.css"
+  },
+  {
+    "revision": "c207ad24e1fbe69eaa01",
+    "url": "/nft-web/static/js/0.a9de6260.chunk.js"
+  },
+  {
+    "revision": "d02ec6d2778552254da8249efee038aa",
+    "url": "/nft-web/static/js/0.a9de6260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c84982c06fcb1777dbef",
+    "url": "/nft-web/static/js/1.a57ed92c.chunk.js"
+  },
+  {
+    "revision": "8628abdbe3d589919841",
+    "url": "/nft-web/static/js/4.e25396a9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/nft-web/static/js/4.e25396a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250cc7633b254c7d1bff",
+    "url": "/nft-web/static/js/5.e9d48a69.chunk.js"
+  },
+  {
+    "revision": "5f65b350d2a7600641da",
+    "url": "/nft-web/static/js/6.f0df79e8.chunk.js"
+  },
+  {
+    "revision": "74175e83c49031d4b5b4",
+    "url": "/nft-web/static/js/7.74493430.chunk.js"
+  },
+  {
+    "revision": "de8477a275d1bc719f2e",
+    "url": "/nft-web/static/js/main.d28949d7.chunk.js"
+  },
+  {
+    "revision": "dc81e18079f6dffe803c",
+    "url": "/nft-web/static/js/runtime-main.9eb601ea.js"
+  },
+  {
+    "revision": "f33bf15d4e137c4f4333ba542e9c8a5c",
+    "url": "/nft-web/static/media/0.f33bf15d.png"
+  },
+  {
+    "revision": "ce84f9fe897e101e979f227944e82c68",
+    "url": "/nft-web/static/media/1.ce84f9fe.png"
+  },
+  {
+    "revision": "1ad9184bfcdd232908971225b6802add",
+    "url": "/nft-web/static/media/2.1ad9184b.png"
+  },
+  {
+    "revision": "6b31d6ecf51e1b5fa32ddbb346802d08",
+    "url": "/nft-web/static/media/3.6b31d6ec.png"
+  },
+  {
+    "revision": "6ddcb2fd6ee4817c7b981c3af3499c84",
+    "url": "/nft-web/static/media/4.6ddcb2fd.gif"
+  },
+  {
+    "revision": "67482643623db5c0f901033c49ca6b1d",
+    "url": "/nft-web/static/media/5.67482643.png"
+  },
+  {
+    "revision": "4d72f9c0e34f201e65a9e285d384a123",
+    "url": "/nft-web/static/media/6.4d72f9c0.png"
+  },
+  {
+    "revision": "8f4c2a55aa3007a3c43961d17096f6cc",
+    "url": "/nft-web/static/media/7.8f4c2a55.png"
+  },
+  {
+    "revision": "f8c2a0872307e2363cb6602efb5d6e2f",
+    "url": "/nft-web/static/media/8.f8c2a087.png"
+  },
+  {
+    "revision": "b48569238eeb0d62de28a5d056b16bef",
+    "url": "/nft-web/static/media/Gabriela-Regular.b4856923.ttf"
+  },
+  {
+    "revision": "9406d0ab606cf8cb91c41b65556bd836",
+    "url": "/nft-web/static/media/Lobster-Regular.9406d0ab.ttf"
+  },
+  {
+    "revision": "2089f31a66e5fcb9197c3e3b5152822a",
+    "url": "/nft-web/static/media/bg01.2089f31a.png"
+  }
+]);
